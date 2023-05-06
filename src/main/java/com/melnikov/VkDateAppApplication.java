650729b91546b2adf844566b5696da5c3e27f3c1
stanislav <rebel1866@yandex.ru>
2023-05-06 14:22:17 +0300

    init

---
+++ src/main/java/com/melnikov/VkDateAppApplication.java
@@ -0,0 +1,13 @@
+package com.melnikov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkDateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkDateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melnikov/VkDateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melnikov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkDateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
