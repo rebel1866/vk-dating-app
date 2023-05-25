ceb9754ec677733a43ae597b50967b57fd9c3821
melnikov <melnikov@isida.by>
2023-05-25 16:04:45 +0300

    17 parse age correctly

--- src/main/java/com/melnikov/util/DateUtil.java
+++ src/main/java/com/melnikov/util/DateUtil.java
@@ -9,6 +9,19 @@ public class DateUtil {
         if (bDate == null) {
             throw new DateTimeException("bDate is null");
         }
+        String[] arr = bDate.split("\\.");
+        if (arr.length != 3) {
+            throw new DateTimeException("Year is not defined");
+        }
+        String day = arr[0];
+        String month = arr[1];
+        if (day.length() == 1) {
+            day = "0" + day;
+        }
+        if (month.length() == 1) {
+            month = "0" + month;
+        }
+        bDate = String.format("%s.%s.%s", day, month, arr[2]);
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate birthDate = LocalDate.parse(bDate, dateTimeFormatter);
         return (short) Math.abs(Period.between(LocalDate.now(), birthDate).getYears());
