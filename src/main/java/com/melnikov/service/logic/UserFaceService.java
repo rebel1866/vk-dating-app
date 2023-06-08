a8a19f37a512aa264f6430caf8b918fe524aa099
stanislav <rebel1866@yandex.ru>
2023-06-08 22:55:59 +0300

    41

---
+++ src/main/java/com/melnikov/service/logic/UserFaceService.java
@@ -0,0 +1,4 @@
+package com.melnikov.service.logic;
+
+public interface UserFaceService {
+}
---
+++ src/main/java/com/melnikov/service/logic/impl/UserFaceServiceImpl.java
@@ -0,0 +1,8 @@
+package com.melnikov.service.logic.impl;
+
+import com.melnikov.service.logic.UserFaceService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserFaceServiceImpl implements UserFaceService {
+}
